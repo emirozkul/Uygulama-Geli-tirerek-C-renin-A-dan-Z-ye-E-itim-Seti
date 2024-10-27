1a2faa00354311314c4a6adb27eb0e758591e6c2
emirozkul <emirrozkul@gmail.com>
2024-10-27 16:54:30 +0300

    String değişkeni kullanılarak, form uygulaması yapıldı.

--- Degiskenler/Form1.cs
+++ Degiskenler/Form1.cs
@@ -16,5 +16,29 @@ namespace Degiskenler
         {
             InitializeComponent();
         }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string isim, soyisim, yas, cinsiyet, meslek;
+
+            isim = textBox1.Text;
+            soyisim = textBox2.Text;
+            yas = maskedTextBox1.Text;
+            meslek = textBox3.Text;
+            cinsiyet = radioButton1.Checked ? "Erkek" : "Kadın";
+
+            listBox1.Items.Add("İsim: " + isim);
+            listBox1.Items.Add("Soyisim: " + soyisim);
+            listBox1.Items.Add("Yaş: " + yas);
+            listBox1.Items.Add("Meslek: " + meslek);
+            listBox1.Items.Add("Cinsiyet: " + cinsiyet);
+            listBox1.Items.Add("-------------------------------");
+            
+        }
     }
 }
