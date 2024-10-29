1ff78fa851d59d4b02337a457cde83c4e798ee69
emirozkul <emirrozkul@gmail.com>
2024-10-29 23:19:35 +0300

    program.cs dosyası tüm formları açacak şekilde düzenlendi

--- Degiskenler/Program.cs
+++ Degiskenler/Program.cs
@@ -16,6 +16,17 @@ namespace Degiskenler
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            basitHesapMakinesi form1 = new basitHesapMakinesi();
+            form1.Show();
+
+            hesaplayici form2 = new hesaplayici();
+            form2.Show();
+
+            KayitFormu form3 = new KayitFormu();
+            form3.Show();
+
+            sinema_bufe_satis_paneli form4 = new sinema_bufe_satis_paneli();
             Application.Run(new sinema_bufe_satis_paneli());
         }
     }
