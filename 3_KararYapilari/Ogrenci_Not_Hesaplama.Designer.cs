5e58a3e96ad752211c4774dbd922f264ec32a35e
emirozkul <emirrozkul@gmail.com>
2024-10-30 06:50:04 +0300

    Öğrenci ortalaması ve harf notu hesaplaması yapan uygulama geliştirildi.

--- 3_KararYapilari/Ogrenci_Not_Hesaplama.Designer.cs
+++ 3_KararYapilari/Ogrenci_Not_Hesaplama.Designer.cs
@@ -28,20 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxVize = new System.Windows.Forms.TextBox();
+            this.textBoxFinal = new System.Windows.Forms.TextBox();
+            this.textBoxProje = new System.Windows.Forms.TextBox();
+            this.textBoxSonuc = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonHesapla = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxHarfNotu = new System.Windows.Forms.TextBox();
+            this.labelGecmeDurumu = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(94, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vize:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(82, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Proje:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(84, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Final:";
+            // 
+            // textBoxVize
+            // 
+            this.textBoxVize.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxVize.Location = new System.Drawing.Point(176, 123);
+            this.textBoxVize.Name = "textBoxVize";
+            this.textBoxVize.Size = new System.Drawing.Size(100, 31);
+            this.textBoxVize.TabIndex = 3;
+            // 
+            // textBoxFinal
+            // 
+            this.textBoxFinal.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxFinal.Location = new System.Drawing.Point(176, 210);
+            this.textBoxFinal.Name = "textBoxFinal";
+            this.textBoxFinal.Size = new System.Drawing.Size(100, 31);
+            this.textBoxFinal.TabIndex = 4;
+            // 
+            // textBoxProje
+            // 
+            this.textBoxProje.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxProje.Location = new System.Drawing.Point(176, 167);
+            this.textBoxProje.Name = "textBoxProje";
+            this.textBoxProje.Size = new System.Drawing.Size(100, 31);
+            this.textBoxProje.TabIndex = 5;
+            // 
+            // textBoxSonuc
+            // 
+            this.textBoxSonuc.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxSonuc.Location = new System.Drawing.Point(159, 19);
+            this.textBoxSonuc.Name = "textBoxSonuc";
+            this.textBoxSonuc.ReadOnly = true;
+            this.textBoxSonuc.Size = new System.Drawing.Size(100, 31);
+            this.textBoxSonuc.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(46, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Sonuç:";
+            // 
+            // buttonHesapla
+            // 
+            this.buttonHesapla.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonHesapla.Location = new System.Drawing.Point(112, 446);
+            this.buttonHesapla.Name = "buttonHesapla";
+            this.buttonHesapla.Size = new System.Drawing.Size(134, 57);
+            this.buttonHesapla.TabIndex = 8;
+            this.buttonHesapla.Text = "Hesapla";
+            this.buttonHesapla.UseVisualStyleBackColor = true;
+            this.buttonHesapla.Click += new System.EventHandler(this.buttonHesapla_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSalmon;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(-15, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(408, 82);
+            this.panel1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(27, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(363, 31);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Öğrenci Notu Hesaplama";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(4, 60);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Harf Notu:";
+            // 
+            // textBoxHarfNotu
+            // 
+            this.textBoxHarfNotu.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxHarfNotu.Location = new System.Drawing.Point(159, 57);
+            this.textBoxHarfNotu.Name = "textBoxHarfNotu";
+            this.textBoxHarfNotu.ReadOnly = true;
+            this.textBoxHarfNotu.Size = new System.Drawing.Size(100, 31);
+            this.textBoxHarfNotu.TabIndex = 10;
+            // 
+            // labelGecmeDurumu
+            // 
+            this.labelGecmeDurumu.AutoSize = true;
+            this.labelGecmeDurumu.Font = new System.Drawing.Font("Georgia", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelGecmeDurumu.Location = new System.Drawing.Point(134, 403);
+            this.labelGecmeDurumu.Name = "labelGecmeDurumu";
+            this.labelGecmeDurumu.Size = new System.Drawing.Size(78, 25);
+            this.labelGecmeDurumu.TabIndex = 12;
+            this.labelGecmeDurumu.Text = "label7";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.PeachPuff;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.textBoxHarfNotu);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.textBoxSonuc);
+            this.panel2.Location = new System.Drawing.Point(28, 271);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(317, 111);
+            this.panel2.TabIndex = 13;
             // 
             // Ogrenci_Not_Hesaplama
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(386, 532);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.labelGecmeDurumu);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonHesapla);
+            this.Controls.Add(this.textBoxProje);
+            this.Controls.Add(this.textBoxFinal);
+            this.Controls.Add(this.textBoxVize);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Ogrenci_Not_Hesaplama";
             this.Text = "Öğrenci Not Hesaplama";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxVize;
+        private System.Windows.Forms.TextBox textBoxFinal;
+        private System.Windows.Forms.TextBox textBoxProje;
+        private System.Windows.Forms.TextBox textBoxSonuc;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonHesapla;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxHarfNotu;
+        private System.Windows.Forms.Label labelGecmeDurumu;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- 3_KararYapilari/Ogrenci_Not_Hesaplama.cs
+++ 3_KararYapilari/Ogrenci_Not_Hesaplama.cs
@@ -15,6 +15,85 @@ namespace _3_KararYapilari
         public Ogrenci_Not_Hesaplama()
         {
             InitializeComponent();
+            labelGecmeDurumu.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonHesapla_Click(object sender, EventArgs e)
+        {
+            int vize = Convert.ToInt32(textBoxVize.Text);
+            int proje = Convert.ToInt32(textBoxProje.Text);
+            int final = Convert.ToInt32(textBoxFinal.Text);
+
+            double ortalama = (vize * 0.4) + (proje * 0.2) + (final * 0.4);
+
+            textBoxSonuc.Text = ortalama.ToString();
+
+            labelGecmeDurumu.Visible = true;
+
+            if (ortalama >= 90)
+            {
+                textBoxHarfNotu.Text = "AA";
+                labelGecmeDurumu.Text = "Geçti";
+                textBoxHarfNotu.BackColor = Color.Green;
+                labelGecmeDurumu.BackColor = Color.Green;
+            }
+            else if (ortalama >= 85 && ortalama <= 89)
+            {
+                textBoxHarfNotu.Text = "BA";
+                labelGecmeDurumu.Text = "Geçti";
+                textBoxHarfNotu.BackColor = Color.Green;
+                labelGecmeDurumu.BackColor = Color.Green;
+            }
+            else if (ortalama >= 80 && ortalama <= 84)
+            {
+                textBoxHarfNotu.Text = "BB";
+                labelGecmeDurumu.Text = "Geçti";
+                textBoxHarfNotu.BackColor = Color.Green;
+                labelGecmeDurumu.BackColor = Color.Green;
+            }
+            else if (ortalama >= 70 && ortalama <= 79)
+            {
+                textBoxHarfNotu.Text = "CB";
+                labelGecmeDurumu.Text = "Geçti";
+                textBoxHarfNotu.BackColor = Color.Green;
+                labelGecmeDurumu.BackColor = Color.Green;
+            }
+            else if (ortalama >= 60 && ortalama <= 69)
+            {
+                textBoxHarfNotu.Text = "CC";
+                labelGecmeDurumu.Text = "Geçti";
+                textBoxHarfNotu.BackColor = Color.Green;
+                labelGecmeDurumu.BackColor = Color.Green;
+            }
+            else if (ortalama >= 50 && ortalama <= 59)
+            {
+                textBoxHarfNotu.Text = "DC";
+                labelGecmeDurumu.Text = "Geçti";
+                textBoxHarfNotu.BackColor = Color.Green;
+                labelGecmeDurumu.BackColor = Color.Green;
+            }
+            else if (ortalama >= 45 && ortalama <= 49)
+            {
+                textBoxHarfNotu.Text = "DD";
+                labelGecmeDurumu.Text = "Geçti";
+                textBoxHarfNotu.BackColor = Color.Green;
+                labelGecmeDurumu.BackColor = Color.Green;
+            }
+            else
+            {
+                textBoxHarfNotu.Text = "FF";
+                labelGecmeDurumu.Text = "Kaldı";
+                textBoxHarfNotu.BackColor = Color.Red;
+                labelGecmeDurumu.BackColor = Color.Red;
+            }
+            
+                
+           
         }
     }
 }
